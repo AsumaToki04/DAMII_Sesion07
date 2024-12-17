8c86c69595fd425b6d6126d9f7a1faaaf5e7a3c1
AsumaToki04 <megamanzxu@hotmail.com>
2024-12-16 19:53:50 -0500

    Initial Commit

---
+++ Sesion07/Sesion07App.swift
@@ -0,0 +1,17 @@
+//
+//  Sesion07App.swift
+//  Sesion07
+//
+//  Created by DAMII on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesion07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
